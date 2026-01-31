ae8e79bd0ae11e7e9f18d0be7d7ec618f4e47834
¨Rodrigo <rsfbernardes@hotmail.com>
2026-01-31 17:16:04 -0300

    test: adiciona cobertura para leitura de CSV com cabeçalho

--- src/test/java/br/com/unipds/chavinho/Chavinho2Test.java
+++ src/test/java/br/com/unipds/chavinho/Chavinho2Test.java
@@ -22,4 +22,24 @@ public class Chavinho2Test {
         Assertions.assertTrue(lista.getFirst().getClass().isRecord());
     }
 
+    @Test
+    void deveLerCsvComCabecalhoIgnorandoPrimeiraLinha() {
+        String csv = "src/test/resources/unipds-disciplinas.csv";
+
+        Chavinho2 chavinho2 = new Chavinho2();
+        List<Disciplina> lista = chavinho2.leCsvDeArquivo(csv, ",", true, Disciplina.class);
+
+        Assertions.assertNotNull(lista);
+        Assertions.assertEquals(11, lista.size());
+
+        Disciplina primeiro = lista.getFirst();
+        Assertions.assertEquals(0, primeiro.numero());
+
+        Disciplina ultimo =  lista.getLast();
+        Assertions.assertEquals("Como Atrair as Melhores Vagas do Mercado ", ultimo.nome());
+        Assertions.assertTrue(ultimo.getClass().isRecord());
+    }
+
+
+
 }
